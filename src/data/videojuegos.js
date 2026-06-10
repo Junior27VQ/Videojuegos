cf46fb744f74479817e4bbf35e1e29a47518db29
Junior27VQ <favq1999@outlook.com>
2026-06-10 17:17:18 -0500

    create the dummy data file

---
+++ src/data/videojuegos.js
@@ -0,0 +1,84 @@
+const data = [
+    {
+        "id": 1,
+        "titulo": "The Legend of Zelda: Breath of the Wild",
+        "genero": "Aventura",
+        "plataforma": "Nintendo Switch",
+        "lanzamiento": "2017-03-03",
+        "precio": 59.99,
+        "disponible": true,
+        "progreso": 0.85
+    },
+    {
+        "id": 2,
+        "titulo": "God of War",
+        "genero": "Aventura",
+        "plataforma": "PlayStation 4",
+        "lanzamiento": "2018-04-20",
+        "precio": 39.99,
+        "disponible": true,
+        "progreso": 0.60
+    },
+    {
+        "id": 3,
+        "titulo": "Spider-Man",
+        "genero": "Aventura",
+        "plataforma": "PlayStation 4",
+        "lanzamiento": "2018-09-07",
+        "precio": 49.99,
+        "disponible": true,
+        "progreso": 0.40
+    },
+    {
+        "id": 4,
+        "titulo": "Red Dead Redemption 2",
+        "genero": "Acción",
+        "plataforma": "Multiplataforma",
+        "lanzamiento": "2018-10-26",
+        "precio": 59.99,
+        "disponible": true,
+        "progreso": 0.75
+    },
+    {
+        "id": 5,
+        "titulo": "The Witcher 3: Wild Hunt",
+        "genero": "RPG",
+        "plataforma": "Multiplataforma",
+        "lanzamiento": "2015-05-19",
+        "precio": 39.99,
+        "disponible": true,
+        "progreso": 0.90
+    },
+    {
+        "id": 6,
+        "titulo": "Cyberpunk 2077",
+        "genero": "RPG",
+        "plataforma": "Multiplataforma",
+        "lanzamiento": "2020-12-10",
+        "precio": 59.99,
+        "disponible": true,
+        "progreso": 0.50
+    },
+    {
+        "id": 7,
+        "titulo": "Hollow Knight",
+        "genero": "Metroidvania",
+        "plataforma": "Multiplataforma",
+        "lanzamiento": "2017-02-21",
+        "precio": 14.99,
+        "disponible": true,
+        "progreso": 0.30
+    },
+    {
+        "id": 8,
+        "titulo": "Hades",
+        "genero": "Roguelike",
+        "plataforma": "Multiplataforma",
+        "lanzamiento": "2020-09-17",
+        "precio": 19.99,
+        "disponible": true,
+        "progreso": 0.65
+    }
+]
+
+export default data;
